4a025a2dc6412b017892d06362d64d8387dd4d6a
Maksymilian <max.szewczyk@interia.pl>
2023-09-26 23:28:31 +0200

    .

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 const footerYear = document.querySelector('.footer__year')
 
-// // Footer year
+// Footer year
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
