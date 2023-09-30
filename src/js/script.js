cd2c0b844b3888a6aef213e66321dd8cffd3c02a
Maksymilian <max.szewczyk@interia.pl>
2023-09-30 23:58:07 +0200

    Fixed navigation and header button, added animations and reduced image sizes (webp)

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,48 @@
-const footerYear = document.querySelector('.footer__year')
+const nav = document.querySelector('.nav')
+const allSections = document.querySelectorAll('.section')
+const navItems = document.querySelectorAll('.nav__item')
+const navItem = document.querySelector('.nav__item')
+const navBtn = document.querySelector('.nav__hamburger-btn')
+
+// Nav
+const toggleNav = () => {
+	nav.classList.toggle('nav--active')
+	document.body.classList.toggle('overflow-hidden')
+	nav.classList.toggle('nav--animation')
+}
+const toggleLinks = e => {
+	nav.classList.remove('nav--active')
+	document.body.classList.remove('overflow-hidden')
+	e.stopPropagation()
+}
+
+window.addEventListener('scroll', () => {
+	allSections.forEach(section => {
+		let top = window.scrollY
+		let offset = section.offsetTop - 90
+		let height = section.offsetHeight
+		let id = section.getAttribute('id')
+		if (top > offset && top <= offset + height) {
+			navItems.forEach(links => {
+				links.classList.remove('nav__item--active')
+				if (id == links.getAttribute('href').slice(1)) {
+					links.classList.add('nav__item--active')
+				}
+			})
+		}
+	})
+})
+navBtn.addEventListener('click', toggleNav)
+navItems.forEach(item => {
+	item.addEventListener('click', toggleLinks)
+})
+// End of Nav
 
 // Footer year
+const footerYear = document.querySelector('.footer__year')
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
 }
 handleCurrentYear()
+// End of Footer year
